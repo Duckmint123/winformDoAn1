648d756d056e97b04aeee73719d80cb10f42c520
Duckmint123 <minhvu1907mv@gmail.com>
2025-06-19 22:30:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace winformDoAn1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.lblCauHoi = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(44, 286);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(105, 54);
+            this.btnA.TabIndex = 0;
+            this.btnA.Text = "button1";
+            this.btnA.UseVisualStyleBackColor = true;
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(192, 286);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(119, 54);
+            this.btnB.TabIndex = 1;
+            this.btnB.Text = "button2";
+            this.btnB.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(368, 286);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(130, 54);
+            this.btnC.TabIndex = 2;
+            this.btnC.Text = "button3";
+            this.btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(575, 286);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(136, 54);
+            this.btnD.TabIndex = 3;
+            this.btnD.Text = "button4";
+            this.btnD.UseVisualStyleBackColor = true;
+            // 
+            // lblCauHoi
+            // 
+            this.lblCauHoi.AutoSize = true;
+            this.lblCauHoi.Location = new System.Drawing.Point(171, 51);
+            this.lblCauHoi.Name = "lblCauHoi";
+            this.lblCauHoi.Size = new System.Drawing.Size(44, 16);
+            this.lblCauHoi.TabIndex = 4;
+            this.lblCauHoi.Text = "label1";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(147, 387);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(44, 16);
+            this.lblKetQua.TabIndex = 5;
+            this.lblKetQua.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.lblCauHoi);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Label lblCauHoi;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace winformDoAn1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=localhost;Initial Catalog=DoAnQuizGame;Integrated Security=True";
+        DataTable cauHoiTable;
+        int cauHoiIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCauHoi();
+            HienThiCauHoi();
+        }
+        void LoadCauHoi()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM CauHoi WHERE ChuDe = N'Lịch sử'";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                cauHoiTable = new DataTable();
+                adapter.Fill(cauHoiTable);
+            }
+        }
+        void HienThiCauHoi()
+        {
+            if (cauHoiIndex >= cauHoiTable.Rows.Count)
+            {
+                lblCauHoi.Text = "Hết câu hỏi rồi!";
+                btnA.Enabled = btnB.Enabled = btnC.Enabled = btnD.Enabled = false;
+                return;
+            }
+
+            DataRow row = cauHoiTable.Rows[cauHoiIndex];
+            lblCauHoi.Text = row["NoiDung"].ToString();
+            btnA.Text = "A. " + row["A"].ToString();
+            btnB.Text = "B. " + row["B"].ToString();
+            btnC.Text = "C. " + row["C"].ToString();
+            btnD.Text = "D. " + row["D"].ToString();
+
+            lblKetQua.Text = ""; // xóa kết quả cũ
+        }
+        void KiemTraDapAn(string dapAnChon)
+        {
+            DataRow row = cauHoiTable.Rows[cauHoiIndex];
+            string dapAnDung = row["DapAn"].ToString();
+
+            if (dapAnChon == dapAnDung)
+                lblKetQua.Text = "✅ Chính xác!";
+            else
+                lblKetQua.Text = "❌ Sai rồi! Đáp án đúng là: " + dapAnDung;
+        }
+
+        private void btnA_Click(object sender, EventArgs e) => KiemTraDapAn("A");
+        private void btnB_Click(object sender, EventArgs e) => KiemTraDapAn("B");
+        private void btnC_Click(object sender, EventArgs e) => KiemTraDapAn("C");
+        private void btnD_Click(object sender, EventArgs e) => KiemTraDapAn("D");
+
+        private void btnCauTiepTheo_Click(object sender, EventArgs e)
+        {
+            cauHoiIndex++;
+            HienThiCauHoi();
+        }
+    }
+}
